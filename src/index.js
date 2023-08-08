2f7faf0d91391b581b378a60175f743af4d7dcfb
sixtynine696969 <134623948+sixtynine696969@users.noreply.github.com>
2023-08-08 13:44:06 +0200

    Add a note on what to do

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// basic blueprint - add functionality, make it a class or something.
+function todoItem(title, description, dueDate, priority, notes, checkList) {
+   return { title, description, dueDate, priority, notes, checkList };
+}
+
+// notes should have a dropdown menu and have a <li> element listing each note.
